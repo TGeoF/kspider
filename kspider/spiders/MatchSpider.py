563ca6e66069415dd50536b7d0ebe975a3b7739f
Tom Freischläger <tom@freischlaeger.de>
2020-02-20 16:48:07 +0100

    change css selector MatchSpider 'matchday'

--- kspider/spiders/MatchSpider.py
+++ kspider/spiders/MatchSpider.py
@@ -6,7 +6,7 @@ from kspider.items import Match
 class MatchSpider(scrapy.Spider):
     name = "MatchSpider"
 
-    def start_requests(self): 
+    def start_requests(self):
         yield scrapy.Request('https://www.kicker.de/{}/spieltag/{}/{}'.format(self.league, self.season, self.start))
 
     def parse(self, response):
@@ -32,23 +32,21 @@ class MatchSpider(scrapy.Spider):
 
         m = Match()
 
-
         stadion = response.css('.kick__gameinfo__item--game-preview '
-                                    ':nth-child(3) p::text').getall()
+                               ':nth-child(3) p::text').getall()
         if len(stadion) > 1:
             m['stadium'] = " ".join(stadion[1].strip().split())
         else:
             m['stadium'] = ''
         m['matchID'] = response.url.split('/')[-3]
 
-
         datestring = extract_with_css('.kick__gameinfo__item--game-preview '
-                                         ':nth-child(2) p::text')
+                                      ':nth-child(2) p::text')
         date = datetime.strptime(datestring, '%d.%m.%Y, %H:%M')
         m['datetime'] = date
         m['league'] = self.league
         m['season'] = self.season
-        m['matchday'] = extract_with_css('div.kick__v100-scoreboardInfo::text')
+        m['matchday'] = extract_with_css('.kick__v100-scoreboardInfo a::text')
         m['homeTeam'] = extract_with_css(
             'div.kick__v100-gameCell__team__name:nth-child(1)::text')
         m['awayTeam'] = extract_with_css(
@@ -58,8 +56,9 @@ class MatchSpider(scrapy.Spider):
         m['awayGoals'] = extract_with_css(
             'div.kick__v100-scoreBoard div:nth-child(1) div:nth-child(3)::text')
         m['referee'] = extract_with_css('.kick__gameinfo__person a::text')
-        m['attendance'] = extract_with_css('.kick__gameinfo__item--game-preview .kick__tabular-nums p::text')
-        
+        m['attendance'] = extract_with_css(
+            '.kick__gameinfo__item--game-preview .kick__tabular-nums p::text')
+
         tl = response.css('.kick__game-timeline *::text').getall()
         for t in range(len(tl)):
             tl[t] = tl[t].strip()
