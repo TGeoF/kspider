55305b8a385115b394c1fad458f5fe8c794152aa
Tom Freischläger <tom@freischlaeger.de>
2020-02-22 19:07:52 +0100

    establish variables in main script

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-process = CrawlerProcess(get_project_settings())
+season = '2019-20'
+league = '1-bundesliga'
+start = 1
+recursive = True
 
-process.crawl('matches', season='2019-20',
-              league='1-bundesliga', start=1, recursive=True)
+process = CrawlerProcess(get_project_settings())
+process.crawl('matches', season=season,
+              league=league, start=start, recursive=recursive)
 process.start()
